c6e9056e6e9bcb41ece2c4f9a468930ed6b0a463
iniJesen <120288897+iniJesen@users.noreply.github.com>
2023-10-16 18:37:12 +0700

    Add Image

--- coba_apk/lib/main.dart
+++ coba_apk/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: "Jesen Ong",
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: firstScreen());
+        home: WidgetDemo();
   }
 }
 
@@ -42,4 +42,62 @@ class firstScreen extends StatelessWidget {
       ),
     );
   }
+},
+
+class WidgetDemo extends StatelessWidget {
+  const WidgetDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Widget demo'),
+      ),
+      body: Column(children: [
+        Container(
+          height: 200,
+          margin: const EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
+          color: Colors.blue,
+          child: const Center(
+            child: Text(
+              'Ini adalah contoh penggunaan container',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text('Tombol Elevated'),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.star, color: Colors.amber),
+            Text('Rating 4.5'),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Image.network(
+            'https://th.bing.com/th/id/OIP.SS9d2mg2_aidN6L-GB3nVgHaFJ?pid=ImgDet&rs=1',
+            width: double.infinity,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ]),
+    );
+  }
 }
+
